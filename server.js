57b815a0c862a999d2d7f0a17f21f1f24b89a7c5
Tyler McWilliam <tylermcwilliam@gmail.com>
2019-09-27 02:10:33 +0800

    updated package.json

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
-const express = require('express');
-const path = require('path');
-const port = process.env.PORT || 8080;
-const app = express();
+var express = require('express');
+var app     = express();
 
-// the __dirname is the current directory from where the script is running
-app.use(express.static(__dirname));
+app.set('port', (process.env.PORT || 5000));
 
-// send the user to index html page inspite of the url
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'index.html'));
+//For avoidong Heroku $PORT error
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
 });
-
-app.listen(port);
